b555c8f7c4c4a9d460f7e991b64853bef04f1540
yoshihere123 <yoshi2006k@gmail.com>
2025-10-16 13:46:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from keep_alive import keep_alive
 status = "dnd" #online/dnd/idle
 
 GUILD_ID = 961795359544328203
-CHANNEL_ID = 1300144312121688198
+CHANNEL_ID = 1418590464206049391
 SELF_MUTE = False
 SELF_DEAF = False
 
